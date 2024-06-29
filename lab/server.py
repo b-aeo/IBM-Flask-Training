d9617ad96b7c3ef2cd18fda2fda649b376966e17
b-aeo <bradespinoza.o@hotmail.com>
2024-06-29 10:33:52 -0400

    commiting initial server file for the flask application

---
+++ lab/server.py
@@ -0,0 +1,16 @@
+
+##import the flask library with flask and jsonify modules
+from flask import Flask, jsonify
+## Creating an application instance using the flask funciton and __name__ variable
+## This stores the instance in the app variable
+app = Flask(__name__)
+##Defining the route path for the function index
+@app.route("/")
+def index():
+    return "hello world"
+##Defining the route path for the function no_content
+@app.route("/no_content")
+def no_content():
+##Using the jsonify function to return a message in json
+    return jsonify( message = "No content found")
+
